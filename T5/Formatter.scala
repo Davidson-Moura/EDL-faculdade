5cd55a511ce5e4c377723417be62f156d8993207
Davidson-Moura <43145031+Davidson-Moura@users.noreply.github.com>
2022-04-10 12:36:47 -0300

    Update Formatter.scala

--- T5/Formatter.scala
+++ T5/Formatter.scala
@@ -4,10 +4,10 @@ object Formatter {
     def formatAddress (label: String): String =  {
         self: Base =>   //Especifica que o 'this' se referência a um objeto do tipo Base
 
-        if (label == 'ShipTo') {
-            return this.getText('Commom.ShipToDefault'); //Endereço de envio padrão
+        if (label == "ShipTo") {
+            return this.getText("Commom.ShipToDefault"); //Endereço de envio padrão
         } else {
-            return this.getText('Commom.BillToDefault'); //Endereço de cobrança padrão
+            return this.getText("Commom.BillToDefault"); //Endereço de cobrança padrão
         }
     }
     ...
